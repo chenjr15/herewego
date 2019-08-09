cd71518c701ccca3dadd3b722252d10461c24554
chenjr15 <chenjr15@live.cn>
2019-08-09 09:52:19 +0800

    加入插入，合并，删除未完成

--- data_structure/tree/bst.go
+++ data_structure/tree/bst.go
@@ -1,5 +1,7 @@
 package tree
 
+import "fmt"
+
 // BST binary search tree
 type BST = Node
 
@@ -14,8 +16,57 @@ func MakeBST(vals []int) *Node {
 	return bst
 }
 
+// 将一棵树合并入另一颗二叉树，不改变其性质
+func (bt *BST) MergeBST(anotherBT *BST) (father *Node) {
+	if bt == nil {
+		return anotherBT
+	}
+	add := func(n *Node, level int) {
+		n.Left = nil
+		n.Right = nil
+		bt.InsertBST(n)
+
+	}
+	anotherBT.LevelOrderTraversal(add)
+
+	return bt
+
+}
+
 // InsertBST 将数据插入二叉排序树，不改变其性质
-func (bt *BST) InsertBST(val int) *Node {
+func (bt *BST) InsertBST(node *Node) *Node {
+	if bt == nil {
+		return nil
+	}
+	if bt == node {
+		return bt
+	}
+	for bt != nil {
+		if node.Val > bt.Val {
+			if bt.Right != nil {
+				bt = bt.Right
+			} else {
+				bt.Right = node
+				bt = nil
+			}
+
+		} else {
+			if bt.Left != nil {
+				bt = bt.Left
+
+			} else {
+				bt.Left = node
+				bt = nil
+			}
+		}
+	}
+
+	return node
+
+}
+
+// InsertBST 将数据插入二叉排序树，不改变其性质
+func (bt *BST) AddBST(val int) *Node {
 	if bt == nil {
 		return nil
 	}
@@ -42,4 +93,79 @@ func (bt *BST) InsertBST(val int) *Node {
 
 	return node
 
+}
+
+// Search 根据指返回对应结点，查找失败返回nil
+func (bt *BST) Search(val int) (node *BST) {
+	for bt != nil {
+		if val == bt.Val {
+			node = bt
+			break
+		} else if val > bt.Val {
+			bt = bt.Right
+
+		} else if val < bt.Val {
+			bt = bt.Left
+		}
+	}
+
+	return
+
+}
+
+// SearchFather 根据指返回对应结点的父节点，查找失败返回nil
+func (bt *BST) SearchFather(val int) (father *BST) {
+	for bt != nil {
+		if val == bt.Val {
+			return father
+
+		} else if val > bt.Val {
+			father = bt
+			bt = bt.Right
+
+		} else if val < bt.Val {
+			father = bt
+			bt = bt.Left
+		}
+	}
+
+	return nil
+
+}
+
+func (bt *BST) RemoveBST(val int) (toRemove *BST) {
+
+	fmt.Printf("Remove %v\n", val)
+	father := bt.SearchFather(val)
+	if father == nil {
+		return nil
+	}
+	fmt.Printf("father : %v, l: %v, r: %v \n", father, father.Left, father.Right)
+	if father.Left != nil && father.Left.Val == val {
+		toRemove = father.Left
+		if toRemove.Left != nil {
+			father.Left = toRemove.Left
+		}
+
+	} else {
+
+		toRemove = father.Right
+		fmt.Printf("ToRemove :%v ", toRemove)
+		if toRemove.Left != nil {
+			father.Right = toRemove.Left
+		}
+	}
+
+	father.MergeBST(toRemove.Right)
+
+	return toRemove
+
+}
+func (bt *BST) BSTString() string {
+	result := ""
+	appendStr := func(node *Node) {
+		result += fmt.Sprintf("%v ", node)
+	}
+	bt.InOrderTraversal(appendStr)
+	return result
 }
--- data_structure/tree/bst_test.go
+++ data_structure/tree/bst_test.go
@@ -5,15 +5,36 @@ import (
 	"testing"
 )
 
-func TestBST(t *testing.T) {
-	n := 10
-	vals := make([]int, n)
-	flag := -1
-	for i := range vals {
-		flag = -1 * flag
-		vals[i] = i + 3*flag + 3
+var bst *BST
+
+var vals []int
+var fathers []int
+
+func TestMerge(t *testing.T) {
+	t1 := MakeBST([]int{5, 4, 10, 8, 11, 7, 9})
+	t2 := MakeBST([]int{2, 1, 3})
+	fmt.Println(t1.BSTString())
+	fmt.Println(t2.BSTString())
+	t1.MergeBST(t2)
+	t.Log(t1.BSTString())
+	t.Log("\n", t1.TreeString())
+
+}
+func TestRemove(t *testing.T) {
+	t1 := MakeBST([]int{5, 4, 10, 8, 11, 7, 9})
+	t.Log("\n", t1.BSTString())
+	remove := []int{1, 4, 5, 10}
+	for _, k := range remove {
+
+		v := t1.RemoveBST(k)
+
+		t.Log(v)
+		t.Log("\n", t1.BSTString())
 	}
-	bst := MakeBST(vals)
+
+}
+
+func TestBST(t *testing.T) {
 	t.Log(vals)
 	result := ""
 	visit := func(node *Node) {
@@ -22,4 +43,45 @@ func TestBST(t *testing.T) {
 	}
 	bst.InOrderTraversal(visit)
 	t.Log(result)
+
+}
+func TestSearch(t *testing.T) {
+
+	for _, val := range vals {
+		node := bst.Search(val)
+		if node == nil || node.Val != val {
+			t.Errorf("%v misatch got :%v", val, node)
+		}
+	}
+	notExists := []int{0, 2, 4, -2}
+	for _, val := range notExists {
+		node := bst.Search(val)
+		if node != nil {
+			t.Errorf("%v misatch got :%v", val, node)
+		}
+	}
+
+	for i, val := range vals {
+		node := bst.SearchFather(val)
+		if node == nil || node.Val != fathers[i] {
+
+			if node == nil && fathers[i] == -1 {
+				break
+
+			}
+			t.Errorf("%v misatch got :%v", val, node)
+		}
+	}
+
+}
+func init() {
+	n := 10
+	vals = make([]int, n)
+	flag := -1
+	for i := range vals {
+		flag = -1 * flag
+		vals[i] = i + 3*flag + 3
+	}
+	bst = MakeBST(vals)
+	fathers = []int{-1, 6, 6, 1, 8, 3, 10, 8, 12, 10}
 }
